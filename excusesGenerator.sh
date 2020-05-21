86ecfe834a90e37ec4f98a49cd1e38b4dfac69c4
quasarseyfert <wcolley124@gmail.com>
2020-05-21 16:08:22 -0400

    Update excusesGenerator.sh

--- excusesGenerator.sh
+++ excusesGenerator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-line=$(($RANDOM%`grep -c '$' /home/chris/Documents/BOFH/excuses.txt`))
+line=$(($RANDOM%`grep -c '$' ~/Documents/BOFH/excuses.txt`))
 
-sed -e  "${line}q;d" /home/chris/Documents/BOFH/excuses.txt
+sed -e  "${line}q;d" ~/Documents/BOFH/excuses.txt
 
 
 
